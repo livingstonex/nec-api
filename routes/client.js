7ace41a59fdb603e1a1102487aa5ebdcab6898a5
Daniel Livingstone <6tone@Daniels-MacBook-Pro.local>
2022-05-16 19:50:59 +0100

    Folder scaffold

---
+++ routes/client.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { AuthController } = require('../controllers/client');
+
+//== Load Routes ==//
+
+module.exports = router;
