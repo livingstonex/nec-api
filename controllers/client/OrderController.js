47b5fa842a1814990669ef818ebafd57b4ca1879
Daniel Livingstone <6tone@Daniels-MacBook-Pro.local>
2022-06-19 16:11:57 +0100

    Implemented index and create.

--- controllers/client/OrderController.js
+++ controllers/client/OrderController.js
@@ -1,7 +1,46 @@
-const {  } = require('../../models/sql').models;
+const { Order } = require('../../models/sql').models;
 
 module.exports = {
-    async index(req, res, next){
+  async index(req, res, next) {
+    const { offset, limit } = req.pagination();
+    const user = req.user;
 
+    try {
+      const { count, rows } = await Order.findAndCountAll({
+        where: {
+          buyer_id: user.id,
+        },
+        offset,
+        limit,
+      });
+
+      const meta = res.pagination(count, limit);
+
+      return res.ok({ message: 'Success', data: rows, meta });
+    } catch (error) {
+      return next(error);
     }
+  },
+
+  async get(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const order = await Order.findOne({
+        where: {
+          id,
+        },
+      }).catch((err) => console.error(err));
+
+      if (!order) {
+        return res.notFound({ message: 'Order not found!' });
+      }
+
+      return res.ok({ message: 'Success', data: order });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async create(req, res, next) {},
 };
