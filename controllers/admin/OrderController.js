c8d9dc2151749d23563ac057fc8ba928f156af06
Daniel Livingstone <6tone@Daniels-MacBook-Pro.local>
2022-06-20 11:13:23 +0100

    Implemented status update for orders

--- controllers/admin/OrderController.js
+++ controllers/admin/OrderController.js
@@ -1,4 +1,5 @@
 const { Order } = require('../../models/sql').models;
+const Email = require('../../utils/email.utils');
 
 module.exports = {
   async index(req, res, next) {
@@ -12,7 +13,7 @@ module.exports = {
         },
         offset,
         limit,
-        include: ['buyer']
+        include: ['buyer'],
       });
 
       const meta = res.pagination(count, limit);
@@ -34,7 +35,7 @@ module.exports = {
         },
         offset,
         limit,
-        include: ['seller']
+        include: ['seller'],
       });
 
       const meta = res.pagination(count, limit);
@@ -53,7 +54,7 @@ module.exports = {
         where: {
           id,
         },
-        include: ['buyer', 'seller']
+        include: ['buyer', 'seller', 'product'],
       }).catch((err) => console.error(err));
 
       if (!order) {
@@ -61,6 +62,77 @@ module.exports = {
       }
 
       return res.ok({ message: 'Success', data: order });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if (!status) {
+      return res.badRequest({
+        message: 'Please attach a valid status',
+      });
+    }
+
+    const data = {
+      state: status.toUpperCase(),
+    };
+
+    try {
+      const order_status = await Order.update(data, {
+        where: {
+          id,
+        },
+      });
+
+      if (!order_status[0]) {
+        return res.unprocessable({
+          message: 'We could not process the update, please try again!',
+        });
+      }
+
+      const order = await Order.findOne({
+        where: {
+          id,
+        },
+        include: ['product'],
+      });
+
+      const buyer = order.getBuyer();
+      const seller = order.getSeller();
+
+      const buyer_data = {
+        buyer_name: buyer.fullname,
+        product_name: order?.product?.name,
+        status,
+      };
+
+      const seller_data = {
+        seller_name: seller.fullname,
+        product_name: order?.product?.name,
+        status,
+      };
+
+      Email.sendEmailTemplate({
+        to: [{ email: buyer.email, name: buyer.fullname }],
+        templateName: 'order-update-buyer-notification',
+        templateData: buyer_data,
+        subject: 'NEC: Order Status Notification',
+      }).catch(console.error());
+
+      Email.sendEmailTemplate({
+        to: [{ email: seller.email, name: seller.fullname }],
+        templateName: 'order-update-seller-notification',
+        templateData: seller_data,
+        subject: 'NEC: Order Status Notification',
+      }).catch(console.error());
+
+      return res.ok({
+        message: 'Status successfully updated.',
+      });
     } catch (error) {
       return next(error);
     }
