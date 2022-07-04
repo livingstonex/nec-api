3efa4a7292c828c85de6648198c9908c8a56bbe9
Daniel Livingstone <6tone@Daniels-MacBook-Pro.local>
2022-07-04 08:50:31 +0100

    added spaces

--- routes/admin.js
+++ routes/admin.js
@@ -5,47 +5,12 @@ const AdminController = require('../controllers/admin/AdminController');
 const { StatusController } = require('../controllers/admin/product');
 const DomesticMarket = require('../controllers/admin/domestic_market/DomesticMarketController');
 
-/**
- * @swagger
- * /api/admin/register:
- *  post:
- *    description: Register Admin
- *    responses:
- *      '201':
- *        description: Admin created successfully
- *      '400':
- *        description: Bad request
- *      '422':
- *        Admin already exists
- *
- */
 router
   .route('/register')
   .post(protect, authorize(['super_admin']), AdminController.register);
-// router.route('/register').post(protect, authorize(['super_admin']), AdminController.register);
-/**
- * @swagger
- * /api/admin/login:
- *  post:
- *    description: Login Admin
- *    responses:
- *      '200':
- *        description: Admin login successful
- *      '400':
- *        description: Bad request
- *
- */
+
 router.route('/login').post(AdminController.login);
-/**
- * @swagger
- * /api/admin/logout:
- *  get:
- *    description: Login Admin
- *    responses:
- *      '200':
- *        description: Admin logout successful
- *
- */
+
 router.route('/logout').get(AdminController.logout);
 
 router
@@ -55,6 +20,7 @@ router
     authorize(['super_admin', 'admin1', 'admin2', 'admin3']),
     StatusController.update
   );
+
 router
   .route('/domestic/market')
   .post(
@@ -67,6 +33,7 @@ router
     authorize(['super_admin', 'admin1', 'admin2', 'admin3']),
     DomesticMarket.index
   );
+  
 router
   .route('/domestic/market/:id')
   .get(
